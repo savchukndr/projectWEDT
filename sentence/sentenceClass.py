d6a816940568b5fe2162ccfd152fc2ed81533286
przemo509 <przemo509@gmail.com>
2016-06-02 01:10:05 +0200

    custom __str__ and __repr__ (used in list) methods for easy debugging

--- sentence/sentenceClass.py
+++ sentence/sentenceClass.py
@@ -1,7 +1,18 @@
 class Sentence:
-    '''Class Sentence'''
-    def __int__(self):
+    """Class Sentence"""
+
+    LAST_ID = -1
+
+    def __init__(self):
         self.sentenceList = []
+        Sentence.LAST_ID += 1
+        self.id = Sentence.LAST_ID
+
+    def __str__(self, *args, **kwargs):
+        return self.__repr__(args, kwargs)
+
+    def __repr__(self, *args, **kwargs):
+        return "Sentence#{0!r}={1!r}".format(self.id, self.sentenceList)
 
 
 if __name__ == '__main__':
--- sentence/termClass.py
+++ sentence/termClass.py
@@ -1,8 +1,19 @@
 class Term:
-    '''Class Term'''
+    """Class Term"""
+
+    LAST_ID = -1
+
     def __init__(self, word, sentence):
         self.word = word
         self.sentence = sentence
+        Term.LAST_ID += 1
+        self.id = Term.LAST_ID
+
+    def __str__(self, *args, **kwargs):
+        return self.__repr__(args, kwargs)
+
+    def __repr__(self, *args, **kwargs):
+        return "Term#{0!r}={1!r}".format(self.id, self.word)
 
 
 if __name__ == '__main__':
