9e01ad2e512d53ca19b34124dff6831a12e4056c
przemo509 <przemo509@gmail.com>
2016-06-03 06:14:32 +0200

    little cleaning domain classes

--- domain/Sentence.py
+++ domain/Sentence.py
@@ -4,7 +4,7 @@ class Sentence:
     LAST_ID = -1
 
     def __init__(self):
-        self.sentenceList = []
+        self.termList = []
         Sentence.LAST_ID += 1
         self.id = Sentence.LAST_ID
 
@@ -12,11 +12,4 @@ class Sentence:
         return self.__repr__(args, kwargs)
 
     def __repr__(self, *args, **kwargs):
-        return "Sentence#{0!r}={1!r}".format(self.id, self.sentenceList)
-
-
-if __name__ == '__main__':
-    """Test data for sentenceClass.py"""
-    I1 = Sentence()
-    I1.termList = [1, 2, 3]
-    print('Term List =', I1.sentenceList)
+        return "Sentence#{0!r}={1!r}".format(self.id, self.termList)
--- domain/Term.py
+++ domain/Term.py
@@ -1,9 +1,12 @@
+from domain import Sentence
+
+
 class Term:
     """Class Term"""
 
     LAST_ID = -1
 
-    def __init__(self, word, sentence):
+    def __init__(self, word: str, sentence: Sentence):
         self.word = word
         self.sentence = sentence
         Term.LAST_ID += 1
@@ -14,9 +17,3 @@ class Term:
 
     def __repr__(self, *args, **kwargs):
         return "Term#{0!r}={1!r}".format(self.id, self.word)
-
-
-if __name__ == '__main__':
-    """Test data for termClass.py"""
-    I1 = Term('test', 'testSentense')
-    print('Word = {0} | Sentense = {1}'.format(I1.word, I1.sentence))
