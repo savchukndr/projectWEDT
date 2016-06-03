44561f7562b0d46e87edcac0ccbab73457ef93cf
przemo509 <przemo509@gmail.com>
2016-06-03 06:17:11 +0200

    a lot of changes: 1. proper chi2 values 2. faster matrix evaluation thanks to dictionaries instead of endless loops 3. better special characters handling 4. Porter stemmer for english (installed from PIP)

--- groupClass.py
+++ groupClass.py
@@ -1,31 +1,39 @@
-import warnings
 from encodings import utf_8
-import numpy as np
+
+from nltk.stem.porter import PorterStemmer
 from numpy.linalg import norm
-from scipy.stats import chisquare as chisq
 from scipy.stats import entropy
+
 from controler import Controler
-from sentence.sentenceClass import Sentence
-from sentence.termClass import Term
+from domain.Sentence import Sentence
+from domain.Term import Term
 
 
 class Group(Controler):
-    '''Class Group '''
-    def __init__(self):
-        Controler.__init__(self, file='textfiles\\text.txt')
-        self.resList = None  # List without dots and etc. and without stopWords
-        self.stopWordsList = None # StopWord list
-        self.resListSplit = None # List with dots and etc. and with stopWords
-        self.sentenceList = [] # Sentence list
-        self.termList = [] # Term list
-        self.ret30percent = None # Dictionary G
-        self.resListSplitWithDots = None # List with dots and etc.
-        self.Dw = None # Dw = ['konczy', 'sie', 'zdanie'] from D = [('konczy', 2), ('sie', 2), ('zdanie', 2)]
-        self.Gw = None # Thesame like Dw
-        self.res = None # Matrix
+    """Class Group"""
 
-    def readFromStopWords(self, file='textfiles\stopwords.txt'):
+    def __init__(self, language='pl'):
+        Controler.__init__(self, file='textfiles\\text.txt')
+        self.language = language
+        self.special_characters = ['.', '!', '?', ',', '"', '\'', ')', '(', '-', '–']
+        self.end_of_sentence_characters = ['.', '!', '?']
+        self.resList = None  # List without dots and etc. and without stopWords
+        self.stopWordsList = None  # StopWord list
+        self.resListSplit = None  # List with dots and etc. and with stopWords
+        self.sentenceList = []  # Sentence list
+        self.termList = []  # Term list
+        self.ret30percent = None  # Dictionary G
+        self.resListSplitWithDots = None  # List with dots and etc.
+        self.Dw = None  # Dw = ['konczy', 'sie', 'zdanie'] from D = [('konczy', 2), ('sie', 2), ('zdanie', 2)]
+        self.Gw = None  # Thesame like Dw
+        self.wordTermDict = {}  # dictionary [word:(term1, term2, ...)]
+        self.coocurenceMatrix = None  # Matrix
+        self.termSentencesTotalSize = []  # counts all terms in sentence where a term occurs
+        self.chi2Values = []  # list of tuples (word, values of chi squared)
+
+    def readFromStopWords(self):
         """Reading words from stopwords.txt"""
+        file = 'textfiles\polish_stopwords.txt' if self.language == 'pl' else 'textfiles\english_stopwords.txt'
         f = open(file, encoding=utf_8.getregentry().name)
         self.stopWordsList = f.read().split('\n')
         f.close()
@@ -52,32 +60,61 @@ class Group(Controler):
         """Remove StopWords from list"""
         tmp = Group.removeNoneSym(self.resList)
         resList = []
+        stemmer = PorterStemmer()
         for x in tmp:
-            if x[-1] in ['.', '!', '?'] and x[:-1] in self.stopWordsList:
-                resList[-1] += x[-1]
-            elif x in self.stopWordsList:
+            if x[-1] in self.end_of_sentence_characters and x[:-1] in self.stopWordsList:
+                if len(resList) > 0:
+                    resList[-1] += x[-1]
+            elif x in self.stopWordsList or x in self.special_characters:
                 continue
             else:
-                resList.append(x)
+                prefix = ''
+                suffix = ''
+                while len(x) >= 1 and x[0] in self.special_characters:
+                    prefix += x[0]
+                    x = x[1:]
+                while len(x) >= 1 and x[-1] in self.special_characters:
+                    suffix = x[-1] + suffix
+                    x = x[:-1]
+                x_stemmed = x if self.language == 'pl' else stemmer.stem(x)
+                # x_stemmed = morfeusz.analyse(x)
+                x_stemmed = prefix + x_stemmed + suffix  # restore special characters for sentence end retrieving
+                resList.append(x_stemmed)
+
         self.resListSplitWithDots = resList
-        resListWithoutDots = list(map(lambda x: x[:-1] if x[-1] in ['.', '!', '?', ','] else x, resList))
-        self.resList = resListWithoutDots
+
+        # remove special characters
+        self.resList = []
+        for word in resList:
+            while len(word) >= 1 and word[0] in self.special_characters:
+                word = word[1:]
+            while len(word) >= 1 and word[-1] in self.special_characters:
+                word = word[:-1]
+            self.resList.append(word)
 
     def groupSentence(self):
-        """Devide words for terms and then for sentences"""
+        """Divide words for terms and then for sentences"""
         sentenceStart = True
+        currentSentence = None
         for x in self.resListSplitWithDots:
             if sentenceStart:
                 currentSentence = Sentence()
                 sentenceStart = False
-            if x.endswith('.') or x.endswith('!') or x.endswith('?'):
+            if x[-1] in self.end_of_sentence_characters:
                 x = x[:-1]
                 sentenceStart = True
-                currentTerm = Term(x, currentSentence)
-                self.termList.append(currentTerm)
-                continue
+            while len(x) >= 1 and x[0] in self.special_characters:
+                x = x[1:]
+            while len(x) >= 1 and x[-1] in self.special_characters:
+                x = x[:-1]
             currentTerm = Term(x, currentSentence)
+            currentSentence.termList.append(currentTerm)
             self.termList.append(currentTerm)
+
+            # filling wordTermDict
+            if self.wordTermDict.get(currentTerm.word) is None:
+                self.wordTermDict[currentTerm.word] = []
+            self.wordTermDict[currentTerm.word].append(currentTerm)
 
     def remove30percent(self):
         """Create map G == 30% from D"""
@@ -96,27 +133,22 @@ class Group(Controler):
         """Creating matrix"""
         self.Dw = Group.converListTuple(self.ret)
         self.Gw = Group.converListTuple(self.ret30percent)
-        tmp = []
-        self.res = []
-        for i in range(len(self.Dw)):
-            listSenD = []
-            for z in self.termList:
-                if self.Dw[i] == z.word:
-                    listSenD.append(z.sentence)
-            listSenDSet = list(set(listSenD))
-            for k in range(len(self.Gw)):
-                count = 0
-                for y in self.termList:
-                    if y.sentence in listSenDSet:
-                        if y.word == self.Dw[i] and y.word == self.Gw[k]:
-                            break
-                        elif y.word == self.Gw[k]:
-                            count += 1
-                        else:
-                            continue
-                tmp.append(count)
-            self.res.append(tmp)
-            tmp = []
+        wordToIdxDict = {}
+        for i in range(len(self.ret)):
+            wordToIdxDict[self.ret[i][0]] = i
+        # noinspection PyUnusedLocal
+        self.coocurenceMatrix = [[0 for x in range(len(self.Gw))] for y in range(len(self.Dw))]
+        # noinspection PyUnusedLocal
+        self.termSentencesTotalSize = [0 for x in range(len(self.Dw))]
+        for w_idx in range(len(self.Dw)):
+            w = self.Dw[w_idx]
+            for wTerm in self.wordTermDict[w]:  # dla wszystkich termów odpowiadających 'w'
+                self.termSentencesTotalSize[w_idx] += len(wTerm.sentence.termList)
+                for sameSentenceTerm in wTerm.sentence.termList:
+                    g_idx = wordToIdxDict[sameSentenceTerm.word]
+                    if sameSentenceTerm.word != wTerm.word and g_idx < len(self.Gw):
+                        self.coocurenceMatrix[w_idx][g_idx] += 1
+        print(self.termSentencesTotalSize)
 
     def printMatrix(self):
         """Output matrix"""
@@ -134,30 +166,38 @@ class Group(Controler):
         # matrix body
         for i in range(len(self.Dw)):
             print(firstColumnFormat.format(self.Dw[i]), end='')
-            row = self.res[i]
+            row = self.coocurenceMatrix[i]
             for cell in row:
                 print(cellFormat.format(str(cell)), end='')
             print()
 
     def chiKwadrat(self):
-        """Chi-square function"""
-        Dc = [] # Dc = [2, 2, 2] from D = [('konczy', 2), ('sie', 2), ('zdanie', 2)]
+        """Custom chi-square function, as in article"""
+        Dc = []  # Dc = [2, 2, 2] from D = [('konczy', 2), ('sie', 2), ('zdanie', 2)]
         for (i, j) in self.ret30percent:
             Dc.append(j)
         print()
-        print('D =', self.res)
+        print('D =', self.coocurenceMatrix)
         print('Dc =', Dc)
         print()
-        for x in range(len(self.res)):
-            observed = np.array(self.res[x])
-            expected = np.array([1 / len(Dc)] * len(Dc)) * np.sum(observed)
-            warnings.simplefilter('error', RuntimeWarning)  # Filter RuntimeWarning when chi2 called
-            try:
-                chi2 = chisq(observed, expected)
-                print('Statistics = {0} | PValue = {1}'.format(chi2.statistic, chi2.pvalue))
-            except RuntimeWarning:
-                print('Statistics = {0} | PValue = {1}'.format(0, 0))
+        # noinspection PyUnusedLocal
+        for w_idx in range(len(self.coocurenceMatrix)):
+            w = self.coocurenceMatrix[w_idx]
+            chi2Value = 0
+            chi2MaxComponent = 0
+            for g_idx in range(len(w)):
+                fwg = w[g_idx]  # freq(w, g), liczba współwystępowań słów w i g w zdaniach
+
+                nwpg = self.termSentencesTotalSize[w_idx] * self.termSentencesTotalSize[g_idx] / len(self.termList)
+                chi2Component = ((fwg - nwpg) ** 2) / nwpg
+                chi2Value += chi2Component
+                if chi2Component > chi2MaxComponent:
+                    chi2MaxComponent = chi2Component
+            chi2Value -= chi2MaxComponent
+            self.chi2Values.append((self.ret[w_idx][0], chi2Value))
         print()
+        self.chi2Values.sort(key=lambda x: x[1], reverse=True)
+        print(self.chi2Values)
 
     @staticmethod
     def JSD(P, Q):
@@ -171,8 +211,8 @@ class Group(Controler):
     def outPutJSDval(self):
         """Output Jensen-Shannon divergence function results"""
         countStep = 0
-        tmp = self.res
-        for x in self.res:
+        tmp = self.coocurenceMatrix
+        for x in self.coocurenceMatrix:
             if sum(x) == 0:
                 res = 0
                 countStep += 1
@@ -213,4 +253,4 @@ if __name__ == '__main__':
     I2.matrixOfApearanceWords()
     I2.printMatrix()
     I2.chiKwadrat()
-    I2.outPutJSDval()
+    # I2.outPutJSDval()
